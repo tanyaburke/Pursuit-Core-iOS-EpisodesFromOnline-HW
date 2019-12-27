e16e4360231d25ab6da4a07b4042b29096b217a9
tanyaburke <tanyaburke@pursuit.org>
2019-12-27 07:40:03 -0500

    setting up structure for app

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ViewControllers/EpisodeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EpisodeViewController.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Tanya Burke on 12/27/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
